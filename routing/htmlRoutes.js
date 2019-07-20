0d27e68e0ade90f71faa087109b5d3eb6c5f86c4
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-07-20 00:06:09 -0400

    added server

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+var path = require("path");
+
+// Routing
+module.exports = function (app) {
+  app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
