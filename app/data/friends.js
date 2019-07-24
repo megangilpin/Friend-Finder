5ed0fca6b53c0fb9840da0578f5521d40892050b
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-07-23 22:04:54 -0400

    assign complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -103,47 +103,52 @@ var user = [
 
 
 // Puts all the users scores into a single array
-// function userScores(array) {
-//   var scoreArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     scoreArray.push(array[i].scores)
-//   }
-//   return (scoreArray)
-// }
+function userScores(array) {
+  var scoreArray = [];
+  for (var i = 0; i < array.length; i++) {
+    scoreArray.push(array[i].scores)
+  }
+  return (scoreArray)
+}
 
-// // Compares the two user scores then finds the total of the difference between each index
-// function compareArray(arr1, arr2) {
-//   console.log(arr1)
-//   var compatScore = [];
-//   var compatTotal = 0
-//   for (var i = 0; i < arr1.length; i++) {
-//     compatScore.push(Math.abs(arr1[i] - arr2[i]));
-//   }
-//   // console.log("compat score Array: " + compatScore)
-//   for (var i = 0; i < compatScore.length; i++) {
-//     compatTotal += compatScore[i]
-//   }
-//   console.log("compat Total: " + compatTotal)
-//   return (compatTotal)
-// }
+// Compares two users' scores then finds the total of the difference between each score
+function compareArray(arr1, arr2) {
+  console.log(arr1)
+  var compatScore = [];
+  var compatTotal = 0
+  for (var i = 0; i < arr1.length; i++) {
+    compatScore.push(Math.abs(arr1[i] - arr2[i]));
+  }
+  // console.log("compat score Array: " + compatScore)
+  for (var i = 0; i < compatScore.length; i++) {
+    compatTotal += compatScore[i]
+  }
+  console.log("compat Total: " + compatTotal)
+  return (compatTotal)
+}
 
-// function compareFriends(arr1, arr2){
-//   // All users scores
-//   var scoreArray = userScores(arr1);
-//   // Current user
-//   var userScore = arr2[0].scores;
-//   console.log(scoreArray)
-//   console.log("user score: " + userScore)
-//   var compatScores = [];
-// // Compares each users scores with the current users scores to find each total difference between the current user and all other 
-//   scoreArray.forEach(element => {
-//     compatScores.push(compareArray(element, userScore))
-//   });
-//   console.log(compatScores)
-//   // finds the index of the lowest compatible score totals
-//   var bestFriend = compatScores.indexOf(Math.min(...compatScores))
-//   return friendArray[bestFriend]
-// }
+  //Compares the new user and current users' scores to find their compatibilities
+function compareFriends(arr1, arr2){
+  // All users scores
+  var scoreArray = userScores(arr1);
+  // Current user
+  var userScore = arr2.scores;
+  console.log(scoreArray)
+  console.log("user score: " + userScore)
+  var compatScores = [];
+  // Compares each users scores with each current users scores to find the total of the difference between each score
+  scoreArray.forEach(element => {
+    compatScores.push(compareArray(element, userScore))
+  });
+  console.log(compatScores)
+  // finds the index of the lowest compatible score totals
+  var bestFriend = compatScores.indexOf(Math.min(...compatScores))
+  console.log(bestFriend)
+  return friendArray[bestFriend]
+}
 
 
-module.exports = friendArray;
+module.exports = {
+  array: friendArray,
+  compare: compareFriends
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,62 +1,22 @@
 var path = require("path");
-var friendArray = require("../data/friends"); 
+var friends = require("../data/friends"); 
 
 module.exports = function(app) {
 
   app.get("/api/friends", function (req, res) {
-    res.json(friendArray);
+    res.json(friends.array);
   });
 
   app.post("/api/friends", function(req,res){
     var currentUser = req.body
+    // Changes the new user's scores in the scores array from a string into an integer
     var intScores = currentUser.scores.map(parseFloat)
+    // Replaces the scores array with the above array
     currentUser.scores = intScores
     console.log(currentUser)
-    alert("You've got a new friend!")
-    compareFriends(friendArray, currentUser)
-    friendArray.push(currentUser)
+    //Compares the new user and current users' scores to find their compatibilities -- see all logic on the friends.js file
+    res.json(friends.compare(friends.array, currentUser))
+    // adds the new user to the friends Array
+    friends.array.push(currentUser)
   })
 }; 
-
-// Puts all the users scores into a single array
-function userScores(array) {
-  var scoreArray = [];
-  for (var i = 0; i < array.length; i++) {
-    scoreArray.push(array[i].scores)
-  }
-  return (scoreArray)
-}
-
-// Compares the two user scores then finds the total of the difference between each index
-function compareArray(arr1, arr2) {
-  console.log(arr1)
-  var compatScore = [];
-  var compatTotal = 0
-  for (var i = 0; i < arr1.length; i++) {
-    compatScore.push(Math.abs(arr1[i] - arr2[i]));
-  }
-  // console.log("compat score Array: " + compatScore)
-  for (var i = 0; i < compatScore.length; i++) {
-    compatTotal += compatScore[i]
-  }
-  console.log("compat Total: " + compatTotal)
-  return (compatTotal)
-}
-
-function compareFriends(arr1, arr2) {
-  // All users scores
-  var scoreArray = userScores(arr1);
-  // Current user
-  var userScore = arr2.scores;
-  console.log(scoreArray)
-  var compatScores = [];
-  // Compares each users scores with the current users scores to find each total difference between the current user and all other 
-  scoreArray.forEach(element => {
-    compatScores.push(compareArray(element, userScore))
-    console.log("user #" + element + compatScores)
-  });
-  // finds the index of the lowest compatible score totals
-  var bestFriend = compatScores.indexOf(Math.min(...compatScores))
-  console.log(friendArray[bestFriend])
-  return friendArray[bestFriend]
-}
